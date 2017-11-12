df6b30c6a602c26a1f6d5f6ee7d30cf10992b73d
Tom <leetomscut@163.com>
2017-11-12 10:19:42 +0800

    init system

---
+++ src/main/scala/cn/xbed/rpc/Master.scala
@@ -0,0 +1,53 @@
+package cn.xbed.rpc
+
+import akka.actor.{Actor, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by root on 11/12/17.
+  */
+class Master extends Actor{
+
+  println("constructor invoked")
+
+  override def preStart(): Unit = {
+    println("preStart invoked")
+  }
+
+  // receive message
+  override def receive: Receive = {
+    case "connected" => {
+      println("a client connected")
+    }
+
+    case "hello" => {
+      println("hello akka")
+    }
+  }
+}
+
+
+object Master {
+  def main(args: Array[String]): Unit = {
+    // defined host and port
+    val host = args(0)
+    val port = args(1).toInt
+    // create config
+    val configStr =
+      s"""
+         |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
+         |akka.remote.netty.tcp.hostname = "$host"
+         |akka.remote.netty.tcp.port = "$port"
+      """.stripMargin
+    val config = ConfigFactory.parseString(configStr)
+    // create and monitor actor, singleton
+    val actorSystem = ActorSystem("MasterSystem", config)
+    // create actor
+    val master = actorSystem.actorOf(Props(new Master), "Master")
+    //send massage
+    master ! "hello"
+    // exit
+    actorSystem.awaitTermination()
+  }
+
+}
